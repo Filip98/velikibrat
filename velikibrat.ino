d4843007bca7e2062592271e23ba42d47a270748
tarhunthethundergod <127444721+tarhunthethundergod@users.noreply.github.com>
2023-08-01 17:37:27 +0200

    Added support for multiple leds
    backandforthandforthandback

--- velikibrat.ino
+++ velikibrat.ino
@@ -1,15 +1,44 @@
 // IT WORKS
 
-uint8_t pinState = 1; // HIGH
+const int defaultPinNumber = 9;
+const int maximumPinNumber = 12;
+const int defaultDelay = 900;
+
+int pinDirection = 1;
+int pinNumber = defaultPinNumber;
+
+int delayDirection = 1;
+int delayTime = defaultDelay;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(8, OUTPUT);
+  for (int i = defaultPinNumber; i <= maximumPinNumber; i++) {
+    pinMode(i, OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(8, pinState);
-  pinState = !pinState;
-  delay(250);
+  digitalWrite(pinNumber, HIGH);
+
+  if (delayTime <= 0) {
+    delayTime = 0;
+    delayDirection *= -1;
+  }
+
+  if (delayTime >= defaultDelay) {
+    delayTime = defaultDelay; // Reset the delay time once it reaches zero (blink cycle completed)
+    delayDirection *= -1;
+  }
+ 
+  delay(delayTime);
+  digitalWrite(pinNumber, LOW);
+  delay(delayTime);
+
+  delayTime -= 30 * delayDirection;
+  pinNumber += 1 * pinDirection;
+
+  if (pinNumber == maximumPinNumber || pinNumber == defaultPinNumber) {
+    pinDirection *= -1;
+  }
 }
